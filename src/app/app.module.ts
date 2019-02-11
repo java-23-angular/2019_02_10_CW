d7358d9d03b32f605b82f24884fd72a3d3127f84
Gregory Sheygam <sheygam@gmail.com>
2019-02-11 23:51:52 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import {RouterModule, Routes} from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+const appRouters: Routes = [
+  {path:'',component:MainComponent},
+  {path:'about',component:AboutComponent},
+  {path:'**',component:NotFoundComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRouters)
   ],
   providers: [],
   bootstrap: [AppComponent]
